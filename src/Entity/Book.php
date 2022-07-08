d16ff55ddb4e94fb072aa514a80060b5639b3c77
Hugo Benabdelhak <hugo.ben80000@gmail.com>
2022-07-08 21:41:45 +0200

    Create Book Entity and BookController

---
+++ src/Entity/Book.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Author;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Title;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Resume;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->Author;
+    }
+
+    public function setAuthor(string $Author): self
+    {
+        $this->Author = $Author;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getResume(): ?string
+    {
+        return $this->Resume;
+    }
+
+    public function setResume(string $Resume): self
+    {
+        $this->Resume = $Resume;
+
+        return $this;
+    }
+}
