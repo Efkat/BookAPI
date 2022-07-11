a27c3269e82be7061708077c3ad405bbb3f3b44c
Hugo Benabdelhak <hugo.ben80000@gmail.com>
2022-07-11 18:25:16 +0200

    Install Serializer & Create Route for books lists

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class BookController extends AbstractController
 {
-    #[Route('/book', name: 'app_book')]
-    public function index(): JsonResponse
+    #[Route('/api/books', name: 'api_getBooks', methods: 'GET')]
+    public function getBooks(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/BookController.php',
-        ]);
+        $jsonBooks = $bookRepository->findAll();
+        $books = $serializer->serialize($jsonBooks, 'json');
+
+        return new JsonResponse($books, Response::HTTP_OK, [], true);
     }
 }
