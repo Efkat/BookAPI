ac445394eade7a595573522619735c5f340b3c44
Hugo Benabdelhak <hugo.ben80000@gmail.com>
2022-07-11 18:43:16 +0200

    Create request to get a specific book

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -14,9 +14,20 @@ class BookController extends AbstractController
     #[Route('/api/books', name: 'api_getBooks', methods: 'GET')]
     public function getBooks(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
     {
-        $jsonBooks = $bookRepository->findAll();
-        $books = $serializer->serialize($jsonBooks, 'json');
+        $bookList = $bookRepository->findAll();
+        $books = $serializer->serialize($bookList, 'json');
 
         return new JsonResponse($books, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/book/{id}', name: 'api_getBookX', methods: 'GET')]
+    public function getBookX(int $id, BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse{
+        $book = $bookRepository->find($id);
+
+        if($book){
+            $jsonBook = $serializer->serialize($book, 'json');
+            return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 }
